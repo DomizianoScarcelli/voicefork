01879faa32d57c3ac414c8bf4e6053d93f5ddb56
CorsiDanilo <corsi.1742375@studenti.uniroma1.it>
2023-04-12 09:03:42 +0200

    Fixing imports and smanetting

--- frontend/src/views/Settings/styles.js
+++ frontend/src/views/Settings/styles.js
@@ -1,1 +1,13 @@
-import { StyleSheet } from "react-native/types";
+import {StyleSheet} from 'react-native';
+
+export const TextStyle = StyleSheet.create({
+    baseText: {
+        fontSize: 10,
+        color: 'black',
+    }
+})
+
+export const PageColor = StyleSheet.create({
+    primary: 'black',
+    secondary: 'white',
+})
