83b41128b9ef2607ba38ef4a5ba9bd4627141744
Domiziano Scarcelli <scarcelli.domiziano@gmail.com>
2023-04-15 18:41:41 +0200

    Changed folder structure

--- frontend/ios/Shared/Extensions/NSUserActivity+IntentData.swift
+++ frontend/ios/Shared/Extensions/NSUserActivity+IntentData.swift
@@ -11,16 +11,23 @@ import Intents
 extension NSUserActivity {
     
   public static let bookRestaurantActivityType = "com.domizianoscarcelli.intentkit.bookrestaurants"
+  public static let myReservationsActivityType = "com.domizianoscarcelli.intentkit.myreservations"
 
-  
   public static var bookRestaurantActivity: NSUserActivity {
       let userActivity = NSUserActivity(activityType: NSUserActivity.bookRestaurantActivityType)
-      
       userActivity.title = "BookRestaurant"
       userActivity.persistentIdentifier = NSUserActivityPersistentIdentifier(NSUserActivity.bookRestaurantActivityType)
       userActivity.isEligibleForPrediction = true
       userActivity.suggestedInvocationPhrase = "Book Restaurant"
-      
+      return userActivity
+  }
+  
+  public static var myReservationsActivity: NSUserActivity {
+      let userActivity = NSUserActivity(activityType: NSUserActivity.myReservationsActivityType)
+      userActivity.title = "MyReservations"
+      userActivity.persistentIdentifier = NSUserActivityPersistentIdentifier(NSUserActivity.myReservationsActivityType)
+      userActivity.isEligibleForPrediction = true
+      userActivity.suggestedInvocationPhrase = "Show my reservations"
       return userActivity
   }
 }
