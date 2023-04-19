1c838422ab15762890c001929f3d7241f6fbfc46
Alessio Lucciola <alessiolucciola98@hotmail.it>
2023-04-19 19:16:26 +0200

    Some changes

--- backend/gateway/index.ts
+++ backend/gateway/index.ts
@@ -1,4 +1,4 @@
-import express from 'express';
+import express, { Express, Request, Response } from 'express';
 import cors from "cors";
 import proxy from 'express-http-proxy';
 
@@ -11,6 +11,10 @@ app.use(express.json());
 
 app.use('/users', proxy('http://localhost:3001'));
 app.use('/restaurants', proxy('http://localhost:3002'));
+
+app.get("/", (req: Request, res: Response) => {
+	res.send("Gateway index")
+})
 
 app.listen(3000, () => {
     console.log('Gateway is listening to port 3000')
