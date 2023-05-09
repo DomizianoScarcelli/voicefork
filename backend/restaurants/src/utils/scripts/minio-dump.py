4ea426ba491d9c398eb7ed7945e34d80c588116a
Domiziano Scarcelli <scarcelli.domiziano@gmail.com>
2023-05-09 13:25:23 +0200

    Python script to add images to bucket

---
+++ backend/restaurants/src/utils/scripts/minio-dump.py
@@ -0,0 +1,26 @@
+from minio import Minio
+import io
+import os
+from tqdm import tqdm
+
+IMAGES_PATH = "./restaurants-images"
+BUCKET_NAME = "images"
+
+# Create client with access key and secret key with specific region.
+client = Minio(
+    "127.0.0.1:9002",
+    access_key="Pj23d8Ii97VFOBiO",
+    secret_key="AlmpakG9yI3xqFZpuIDyUnFlXCNaeRWK",
+    secure=False  # Otherwise bug
+)
+
+
+def dump_images(images_folder):
+    for index, filename in enumerate(tqdm(os.listdir(images_folder), desc="Uploading files")):
+        image_path = os.path.join(images_folder, filename)
+        client.fput_object(
+            BUCKET_NAME, f"restaurant_image_{index}", image_path)
+
+
+if __name__ == "__main__":
+    dump_images(IMAGES_PATH)
