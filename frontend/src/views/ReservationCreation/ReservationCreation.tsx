6be454b73ce12dc5ba3fd2f6bfd0939823d2c090
Domiziano Scarcelli <scarcelli.domiziano@gmail.com>
2023-05-11 12:52:51 +0200

    Fixed page to make reservation for ios

--- frontend/src/views/ReservationCreation/ReservationCreation.tsx
+++ frontend/src/views/ReservationCreation/ReservationCreation.tsx
@@ -1,21 +1,44 @@
-import React, { useEffect, useState } from 'react'
-import { Alert, Image, Text, TouchableOpacity, View } from 'react-native'
-import { ReservationCreationDetails, Restaurant } from '../../shared/types'
-import { ScrollView } from 'react-native-gesture-handler'
-import { Calendar } from 'react-native-calendars'
-import { styles } from './styles'
-import { Colors } from '../../constants'
-import { Picker } from '@react-native-picker/picker';
-import { constructDateTimeFromString, getCurrentDate, getNextReservableTime, getReservableTimes } from '../../utils/dateTimeUtils'
+import React, {useEffect, useState} from 'react'
+import {Alert, Text, TouchableOpacity, View, SafeAreaView} from 'react-native'
+import {ReservationCreationDetails, Restaurant} from '../../shared/types'
+import {ScrollView} from 'react-native-gesture-handler'
+import {Calendar} from 'react-native-calendars'
+import {styles} from './styles'
+import {Colors} from '../../constants'
+import {Picker} from '@react-native-picker/picker'
+import {
+    constructDateTimeFromString,
+    getCurrentDate,
+    getNextReservableTime,
+    getReservableTimes,
+} from '../../utils/dateTimeUtils'
 import Ionicons from 'react-native-vector-icons/Ionicons'
-import { createReservation } from '../../utils/apiCalls'
-import { useSession } from '../../hooks/useSession'
+import {createReservation} from '../../utils/apiCalls'
+import {useSession} from '../../hooks/useSession'
+import RestaurantImage from '../../components/RestaurantImage/RestaurantImage'
 
-function ReservationCreation({route, navigation}: {route: any, navigation: any}) {
+function ReservationCreation({
+    route,
+    navigation,
+}: {
+    route: any
+    navigation: any
+}) {
     const userId = useSession(navigation)
-    const [restaurantDetails, setRestaurantDetails] = useState<Restaurant>(route.params.restaurant)
-    const [reservationDetails, setReservationDetails] = useState<ReservationCreationDetails>({ id_user: 0, id_restaurant: route.params.restaurant.id, dateTime: new Date(), n_people: 1 })
-    const [dateTime, setDateTime] = useState( { date: "", time: getNextReservableTime() })
+    const [restaurantDetails, setRestaurantDetails] = useState<Restaurant>(
+        route.params.restaurant,
+    )
+    const [reservationDetails, setReservationDetails] =
+        useState<ReservationCreationDetails>({
+            id_user: 0,
+            id_restaurant: route.params.restaurant.id,
+            dateTime: new Date(),
+            n_people: 1,
+        })
+    const [dateTime, setDateTime] = useState({
+        date: '',
+        time: getNextReservableTime(),
+    })
     const [reservableTimes, setReservableTimes] = useState<string[]>([])
     const [loading, setLoading] = useState(false)
 
@@ -45,7 +68,10 @@ function ReservationCreation({route, navigation}: {route: any, navigation: any})
 
     const validateIdUser = () => {
         let isValid = true
-        if (reservationDetails.id_user === null || reservationDetails.id_user === 0) {
+        if (
+            reservationDetails.id_user === null ||
+            reservationDetails.id_user === 0
+        ) {
             isValid = false
         }
         return isValid
@@ -53,7 +79,10 @@ function ReservationCreation({route, navigation}: {route: any, navigation: any})
 
     const validateIdRestaurant = () => {
         let isValid = true
-        if (reservationDetails.id_restaurant === null || reservationDetails.id_restaurant === 0) {
+        if (
+            reservationDetails.id_restaurant === null ||
+            reservationDetails.id_restaurant === 0
+        ) {
             isValid = false
         }
         return isValid
@@ -66,38 +95,37 @@ function ReservationCreation({route, navigation}: {route: any, navigation: any})
         const isValidIdUser = validateIdUser()
         const isValidIdRestaurant = validateIdRestaurant()
 
-        if (isValidDate && isValidNPeople && isValidIdUser && isValidIdRestaurant) {
+        if (
+            isValidDate &&
+            isValidNPeople &&
+            isValidIdUser &&
+            isValidIdRestaurant
+        ) {
             createNewReservation()
         } else {
-            Alert.alert(  
-                'Something is wrong',  
-                'Please, check your data and try again',  
-                [  
-                    {text: 'OK'},  
-                ]  
+            Alert.alert(
+                'Something is wrong',
+                'Please, check your data and try again',
+                [{text: 'OK'}],
             )
             setLoading(false)
         }
-
     }
 
     const createNewReservation = async () => {
         let formData = {
-            "id_user": reservationDetails.id_user,
-            "id_restaurant": reservationDetails.id_restaurant,
-            "dateTime": reservationDetails.dateTime,
-            "n_people": reservationDetails.n_people
+            id_user: reservationDetails.id_user,
+            id_restaurant: reservationDetails.id_restaurant,
+            dateTime: reservationDetails.dateTime,
+            n_people: reservationDetails.n_people,
         }
 
         const resultStatus = await createReservation(formData)
         if (resultStatus === 200) {
-            Alert.alert(  
-                'Reservation added successfully',  
-                `Booked a table at ${restaurantDetails.name}, ${dateTime.date} at ${dateTime.time} for ${reservationDetails.n_people}`,  
-                [  
-                    {text: 'OK',
-                    onPress: () => navigation.navigate("Homepage")}
-                ]  
+            Alert.alert(
+                'Reservation added successfully',
+                `Booked a table at ${restaurantDetails.name}, ${dateTime.date} at ${dateTime.time} for ${reservationDetails.n_people}`,
+                [{text: 'OK', onPress: () => navigation.navigate('Homepage')}],
             )
         }
         setLoading(false)
@@ -110,18 +138,32 @@ function ReservationCreation({route, navigation}: {route: any, navigation: any})
     }, [userId])
 
     return (
-        <>
+        <SafeAreaView>
             <ScrollView>
-                <Image source={{uri: 'https://i.pinimg.com/originals/3d/a3/7d/3da37dc6421f978a50e165466f221d72.jpg'}} style={{height: 200}} />
+                <RestaurantImage
+                    imageName={restaurantDetails.imageName}
+                    style={{height: 200}}
+                />
                 <View style={styles.reservationCreationContainer}>
                     <View>
-                        <Text style={styles.reservationCreationTitle}>Booking a table at: </Text>
-                        <Text style={styles.reservationCreationTitle}>{restaurantDetails.name}</Text>
+                        <Text style={styles.reservationCreationTitle}>
+                            Booking a table at:{' '}
+                        </Text>
+                        <Text style={styles.reservationCreationTitle}>
+                            {restaurantDetails.name}
+                        </Text>
                     </View>
                     <View style={styles.restaurantColumn}>
                         <View style={styles.restaurantRow}>
-                            <Ionicons name={"map-outline"} size={20} color={Colors.green} style={{marginRight: 5}}/>
-                            <Text style={styles.restaurantAddress}>{restaurantDetails.address}</Text>
+                            <Ionicons
+                                name={'map-outline'}
+                                size={20}
+                                color={Colors.green}
+                                style={{marginRight: 5}}
+                            />
+                            <Text style={styles.restaurantAddress}>
+                                {restaurantDetails.address}
+                            </Text>
                         </View>
                     </View>
                 </View>
@@ -136,57 +178,101 @@ function ReservationCreation({route, navigation}: {route: any, navigation: any})
                             todayTextColor: Colors.darkGreen,
                             textDisabledColor: Colors.darkGray,
                             arrowColor: Colors.darkGreen,
-                            indicatorColor: Colors.darkGreen
+                            indicatorColor: Colors.darkGreen,
                         }}
                         onDayPress={day => {
                             setDateTime({...dateTime, date: day.dateString})
-                            setReservableTimes(getReservableTimes(new Date(day.dateString)))
-                            setReservationDetails({...reservationDetails, dateTime: constructDateTimeFromString(day.dateString, dateTime.time)})
+                            setReservableTimes(
+                                getReservableTimes(new Date(day.dateString)),
+                            )
+                            setReservationDetails({
+                                ...reservationDetails,
+                                dateTime: constructDateTimeFromString(
+                                    day.dateString,
+                                    dateTime.time,
+                                ),
+                            })
                         }}
                         markedDates={{
-                            [dateTime.date]: {selected: true}
+                            [dateTime.date]: {selected: true},
                         }}
                         minDate={getCurrentDate()}
-                        current={dateTime.date}>
-                    </Calendar>
-                    {dateTime.date !== "" ? (
+                        current={dateTime.date}></Calendar>
+                    {dateTime.date !== '' ? (
                         <>
                             <Text>Select a time:</Text>
                             <Picker
                                 selectedValue={dateTime.time}
                                 onValueChange={itemValue => {
                                     setDateTime({...dateTime, time: itemValue})
-                                    setReservationDetails({...reservationDetails, dateTime: constructDateTimeFromString(dateTime.date, itemValue)})
+                                    setReservationDetails({
+                                        ...reservationDetails,
+                                        dateTime: constructDateTimeFromString(
+                                            dateTime.date,
+                                            itemValue,
+                                        ),
+                                    })
                                 }}
                                 dropdownIconColor={Colors.darkGreen}
-                                style={{backgroundColor: Colors.white}}>
-                                {reservableTimes !== null ? 
-                                    reservableTimes.map((item, index) => (
-                                        <Picker.Item label={item} value={item} key={index} />
-                                )) : ""}
+                                itemStyle={{height: 150}}
+                                style={{
+                                    backgroundColor: Colors.white,
+                                }}>
+                                {reservableTimes !== null
+                                    ? reservableTimes.map((item, index) => (
+                                          <Picker.Item
+                                              label={item}
+                                              value={item}
+                                              key={index}
+                                          />
+                                      ))
+                                    : ''}
                             </Picker>
                             <Text>Select the number of people:</Text>
                             <Picker
                                 selectedValue={reservationDetails.n_people}
                                 onValueChange={itemValue => {
-                                    setReservationDetails({...reservationDetails, n_people: itemValue})
+                                    setReservationDetails({
+                                        ...reservationDetails,
+                                        n_people: itemValue,
+                                    })
                                 }}
+                                itemStyle={{height: 150}}
                                 dropdownIconColor={Colors.darkGreen}
-                                style={{backgroundColor: Colors.white}}>
+                                style={{
+                                    backgroundColor: Colors.white,
+                                }}>
                                 {getReservableSeats().map((item, index) => (
-                                        <Picker.Item label={item.toString()} value={item} key={index} />
+                                    <Picker.Item
+                                        label={item.toString()}
+                                        value={item}
+                                        key={index}
+                                    />
                                 ))}
                             </Picker>
-                            <View style={{justifyContent: "center", alignItems: "center"}}>
-                                <TouchableOpacity style={styles.book_button} onPress={() => validateData()} disabled={loading}>
-                                    <Text style={styles.button_text}>{loading ? "RESERVING A TABLE..." : "CONFIRM BOOKING"}</Text>
+                            <View
+                                style={{
+                                    justifyContent: 'center',
+                                    alignItems: 'center',
+                                }}>
+                                <TouchableOpacity
+                                    style={styles.book_button}
+                                    onPress={() => validateData()}
+                                    disabled={loading}>
+                                    <Text style={styles.button_text}>
+                                        {loading
+                                            ? 'RESERVING A TABLE...'
+                                            : 'CONFIRM BOOKING'}
+                                    </Text>
                                 </TouchableOpacity>
                             </View>
                         </>
-                    ): ""}
+                    ) : (
+                        ''
+                    )}
                 </View>
             </ScrollView>
-        </>
+        </SafeAreaView>
     )
 }
 
