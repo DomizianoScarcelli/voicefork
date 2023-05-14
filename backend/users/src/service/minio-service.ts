db96d064de99535b59b136ed28d1a3f5403e9230
Alessio Lucciola <alessiolucciola98@hotmail.it>
2023-05-14 19:03:32 +0200

    Added Minio support to Users microservice

---
+++ backend/users/src/service/minio-service.ts
@@ -0,0 +1,87 @@
+const Minio = require('minio') //Require because with import there is a strange bug
+import {Client, UploadedObjectInfo} from 'minio'
+import {Readable} from 'stream'
+
+class MinioService {
+    client: Client
+    bucketName: string
+
+    constructor() {
+        // Local configuration
+        if (process.env.USE_MINIO_LOCAL) {
+            if (
+                process.env.MINIO_ACCESS_KEY == undefined ||
+                process.env.MINIO_SECRET_KEY == undefined
+            )
+                throw new Error('Local MINIO credentials not found')
+
+            this.client = new Minio.Client({
+                endPoint: 'minio-users',
+                port: 9001,
+                accessKey: process.env.MINIO_ACCESS_KEY,
+                secretKey: process.env.MINIO_SECRET_KEY,
+                useSSL: false,
+            })
+            this.bucketName = 'avatars'
+        } else {
+            // AWS S3 configuration
+            if (
+                process.env.AWS_ACCESS_KEY == undefined ||
+                process.env.AWS_SECRET_KEY == undefined ||
+                process.env.AWS_SESSION_TOKEN == undefined
+            )
+                throw new Error('AWS credentials not found')
+
+            this.client = new Minio.Client({
+                endPoint: 's3.amazonaws.com',
+                accessKey: process.env.AWS_ACCESS_KEY,
+                secretKey: process.env.AWS_SECRET_KEY,
+                sessionToken: process.env.AWS_SESSION_TOKEN,
+            })
+            this.bucketName = 'voicefork-avatars'
+        }
+    }
+
+    async putObject(objectName: string, object: string | Readable | Buffer) {
+        return this.client.putObject(
+            this.bucketName,
+            objectName,
+            object,
+            (err: Error, objInfo: UploadedObjectInfo) => {
+                if (err) {
+                    console.log(err)
+                }
+                console.log('Object successfully stored. Object info:', objInfo)
+                return objInfo
+            },
+        )
+    }
+
+    async getObject(objectName: string): Promise<Buffer> {
+        const chunks: Buffer[] = []
+
+        const dataStream = await this.client.getObject(
+            this.bucketName,
+            objectName,
+        )
+
+        dataStream.on('data', (chunk: Buffer) => {
+            chunks.push(chunk)
+        })
+
+        return new Promise<Buffer>((resolve, reject) => {
+            dataStream.on('end', () => {
+                const objectData = Buffer.concat(chunks)
+                console.log('Object successfully retrieved:', objectData)
+                resolve(objectData)
+            })
+
+            dataStream.on('error', (error: Error) => {
+                console.error('Error reading object data stream:', error)
+                reject(error)
+            })
+        })
+    }
+}
+
+export default MinioService
--- backend/users/src/utils/minio-dump.py
+++ backend/users/src/utils/minio-dump.py
@@ -2,15 +2,15 @@ from minio import Minio
 import os
 from tqdm import tqdm
 
-IMAGES_PATH = "./compressed-images"
-BUCKET_NAME = "images"
+IMAGES_PATH = "./avatars" # Change the path according to where you put the "avatars" folder
+BUCKET_NAME = "avatars"
 
 
 # Create client with access key and secret key with specific region.
 client = Minio(
-    "localhost:9002",
-    access_key="dabJnZyAC0axbXSg",
-    secret_key="4c8EF9kkMUbdjCPvobvOm4Xoo0rc9FGf",
+    "localhost:9001",
+    access_key="rECSg0kT1xFuRxr2",
+    secret_key="ipkp3GN4aCtn8dyvQM1ouOZTNGpWtoDS",
     secure=False  # Otherwise bug
 )
 
@@ -19,7 +19,7 @@ def dump_images(images_folder):
     for index, filename in enumerate(tqdm(os.listdir(images_folder), desc="Uploading files")):
         image_path = os.path.join(images_folder, filename)
         client.fput_object(
-            BUCKET_NAME, f"restaurant_image_{index}", image_path)
+            BUCKET_NAME, f"avatar_{index}", image_path)
 
 
 if __name__ == "__main__":
