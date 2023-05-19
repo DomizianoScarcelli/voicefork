aa5e8dbeaa2a3a70587ea1a6538f663f78834b70
Domiziano Scarcelli <scarcelli.domiziano@gmail.com>
2023-05-19 17:33:00 +0200

    Corrected minio backup

--- backend/restaurants/db/minio-backup/minio-backup.sh
+++ backend/restaurants/db/minio-backup/minio-backup.sh
@@ -1,7 +1,5 @@
-#otti mc scaricando il binario con curl, cambiandono in eseguibile e spostandolo in bin
-docker exec embeddings sh -c 'mc config host add minio http://localhost:9002 2yt0sIR2x0iSu58E CWPZu7c3DIJM6RT9jTqZQkE7KkyqFvmU && mc mirror --overwrite minio/embeddings /home/dov/voicefork/backend/restaurants/db/embeddings > export.log'
 #!bin/bash
-container_name = minio-restaurants
-backup_path = $(pwd):/backup
+CONTAINER_NAME=minio-restaurants
+BACKUP_PATH=$(pwd):/embeddings
 
-docker run --rm --volumes-from $container_name -v $backup_path ubuntu tar cvf /backup/backup.tar /data
+docker run --rm --volumes-from $CONTAINER_NAME -v $BACKUP_PATH ubuntu tar cvf /embeddings/embeddings.tar /data
--- backend/restaurants/db/minio-backup/minio.restore.sh
+++
@@ -1,5 +0,0 @@
-#!bin/bash
-container_name = minio-restaurants
-backup_path = $(pwd):/backup
-
-docker run --rm --volumes-from $container_name -v $backup_path ubuntu bash -c "cd /data && tar xvf /backup/backup.tar --strip 1"
