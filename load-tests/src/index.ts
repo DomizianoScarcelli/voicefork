2d9c84774ed5050b04d66c384dc32645165b0769
Domiziano Scarcelli <scarcelli.domiziano@gmail.com>
2023-05-24 23:18:58 +0200

    Adding monitoring

---
+++ load-tests/src/index.ts
@@ -0,0 +1,18 @@
+import http from "k6/http"
+import { check, sleep } from "k6"
+import { searchRestaurant, loadRestaurantImages, loadRestaurantsNearby, loadTopRatedRestaurants } from "./utils/pipeline.js"
+
+export const options = {
+	stages: [
+		{ duration: "10s", target: 5 },
+		{ duration: "20s", target: 12 },
+		{ duration: "10", target: 0 },
+	],
+}
+
+export default function() {
+	searchRestaurant()
+	// loadRestaurantsNearby()
+	// loadTopRatedRestaurants()
+	// loadRestaurantImages(40)
+}
