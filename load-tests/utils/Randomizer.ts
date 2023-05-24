db216863d34b447fee548d052949a5fa9fe4525d
Domiziano Scarcelli <scarcelli.domiziano@gmail.com>
2023-05-24 21:44:51 +0200

    Structure of the api calls

---
+++ load-tests/utils/Randomizer.ts
@@ -0,0 +1,38 @@
+import { LatLng } from "../shared/types"
+import { coordinates } from "../data/coordinates"
+import { restaurantNames } from "../data/restaurantsNames"
+
+export class Randomizer {
+	seed: number | undefined
+
+	constructor(seed?: number) {
+		this.seed = seed
+	}
+
+	private seededRandom = () => {
+		if (this.seed) {
+			let x = Math.sin(this.seed) * 10000
+			return x - Math.floor(x)
+		}
+		return Math.random()
+	}
+
+	private randomChoice<T>(collection: T[]): T {
+		const randomIndex = Math.random() * collection.length
+		return collection[randomIndex]
+	}
+
+	getRandomRestaurantName = (): string => {
+		return this.randomChoice(restaurantNames)
+	}
+
+	getRandomCoordinates = (): LatLng => {
+		return this.randomChoice(coordinates)
+	}
+
+	getRandomRestaurantImageName = (): string => {
+		const MAX_NUM = 6000 //Amount of images in the bucket
+		const index = this.seededRandom() * MAX_NUM
+		return `restaurant_image_${index}`
+	}
+}
