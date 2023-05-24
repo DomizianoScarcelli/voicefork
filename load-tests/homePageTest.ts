9f056d6d93ac441ec2be3823f1464f68fa6af432
Domiziano Scarcelli <scarcelli.domiziano@gmail.com>
2023-05-24 21:44:51 +0200

    Structure of the api calls

--- load-tests/homePageTest.ts
+++ load-tests/homePageTest.ts
@@ -1,16 +1,18 @@
 import http from "k6/http"
 import { check, sleep } from "k6"
+import { searchRestaurant, loadRestaurantImages, loadRestaurantsNearby, loadTopRatedRestaurants } from "./utils/pipeline"
 
 export const options = {
 	stages: [
-		{ duration: "10s", target: 10 },
-		{ duration: "20s", target: 50 },
+		{ duration: "10s", target: 5 },
+		{ duration: "20s", target: 12 },
 		{ duration: "10", target: 0 },
 	],
 }
 
 export default function () {
-	const res = http.get("http://localhost:3000/restaurants/search-restaurants?query=blu%20bar&latitude=41.909734&longitude=12.349999&maxDistance=5000&page=1")
-	check(res, { "status was 200": (r) => r.status == 200 })
-	sleep(1)
+	searchRestaurant()
+	loadRestaurantsNearby()
+	loadTopRatedRestaurants()
+	loadRestaurantImages(40)
 }
---
+++ load-tests/utils/pipeline.ts
@@ -0,0 +1,64 @@
+import http from "k6/http"
+import { check, sleep } from "k6"
+import { RESTAURANT_URL } from "../shared/constants"
+import { Randomizer } from "../utils/randomizer"
+
+const randomizer = new Randomizer()
+
+export const loadRestaurantImages = (numImages: number) => {
+	//TODO: make the call directly to te bucket and not to the API
+	let imagesList: string[] = []
+	for (let i = 0; i < numImages; i++) {
+		const imageName = randomizer.getRandomRestaurantImageName()
+		const URL = `${RESTAURANT_URL}/restaurant-image?imageName=${imageName}`
+		const res = http.get(URL)
+	}
+}
+
+export const loadRestaurantsNearby = () => {
+	const params = {
+		coordinates: randomizer.getRandomCoordinates(),
+		maxDistance: 10000,
+		limit: 10,
+	}
+	const { coordinates, limit, maxDistance } = params
+	const { latitude, longitude } = coordinates
+	const URL = `${RESTAURANT_URL}/find-restaurants-nearby?latitude=${latitude}&longitude=${longitude}&limit=${limit}&maxDistance=${maxDistance}`
+	const res = http.get(URL)
+
+	check(res, { "status was 200": (r: Response) => r.status == 200 })
+	sleep(1)
+}
+
+export const loadTopRatedRestaurants = () => {
+	const params = {
+		coordinates: randomizer.getRandomCoordinates(),
+		minRating: 4,
+		maxDistance: 10000,
+		limit: 10,
+	}
+	const { coordinates, limit, maxDistance, minRating } = params
+	const { latitude, longitude } = coordinates
+	const URL = `${RESTAURANT_URL}/find-restaurants-nearby?latitude=${latitude}&longitude=${longitude}&limit=${limit}&minRating=${minRating}&maxDistance=${maxDistance}`
+	const res = http.get(URL)
+
+	check(res, { "status was 200": (r: Response) => r.status == 200 })
+	sleep(1)
+}
+
+export const searchRestaurant = () => {
+	//The user make a search request for a certain restaurant
+	const searchParams = {
+		query: randomizer.getRandomRestaurantName(),
+		coordinates: randomizer.getRandomCoordinates(),
+		maxDistance: 50000,
+		fastSearch: false,
+	}
+	const { query, coordinates, maxDistance, fastSearch } = searchParams
+	const { latitude, longitude } = coordinates
+	const URL = `${RESTAURANT_URL}/search-restaurants?query=${query}&latitude=${latitude}&longitude=${longitude}&maxDistance=${maxDistance}&fastSearch=${fastSearch}`
+	const res = http.get(URL)
+
+	check(res, { "status was 200": (r: Response) => r.status == 200 })
+	sleep(1)
+}
